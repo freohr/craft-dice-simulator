ff218b052118273f0c4ba0780f50e3e421386eaf
Stephen FAURE <faure.stephen@gmail.com>
2017-05-04 17:50:32 +0200

    Ajout sauvegarde des jets et gestion des crits

--- dice-roller.py
+++ dice-roller.py
@@ -3,10 +3,20 @@ import random
 total = 1000
 current = 0
 count = 0
+rolls = []
+crits = []
 
 while current < total:
-    current += random.randrange(99)
+    roll = random.randrange(99)
+    rolls.append(roll)
+    if roll % 11 == 0:
+        crits.append(roll)
+        current += 2 * roll
+    else:
+        current += roll
     count += 1
 
 print "Total obtenu : " + str(current)
 print str(count) + " jets pour atteindre " + str(total)
+print "Jets : " + str(rolls)
+print "Critiques : " + str(crits)
